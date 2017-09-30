7339ca6d67820a88ae4e2bdc434bd1d8f59c7674
Bogdan Arabadzhi <barabadzhi@iba.by>
2017-09-30 23:47:07 +0200

    Add implementing-display exercise

---
+++ implementing-display/src/main.rs
@@ -0,0 +1,48 @@
+#![allow(dead_code)]
+
+use std::fmt::{self, Display};
+
+#[derive(Debug, PartialEq)]
+enum FileState {
+    Open,
+    Closed,
+}
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+    state: FileState,
+}
+
+impl Display for FileState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Closed => write!(f, "CLOSED"),
+        }
+    }
+}
+
+impl Display for File {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<{} ({})>", self.name, self.state)
+    }
+}
+
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+            state: FileState::Closed,
+        }
+    }
+}
+
+fn main() {
+    let f6 = File::new("f6.txt");
+    // ...
+    println!("{:?}", f6);
+    println!("{}", f6);
+}
