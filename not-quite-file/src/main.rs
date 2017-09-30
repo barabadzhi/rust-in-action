673a99a235348e0f2c037563832bf6f9876231b4
Bogdan Arabadzhi <barabadzhi@iba.by>
2017-09-30 21:38:46 +0200

    Add not-quite-file exercise

---
+++ not-quite-file/src/main.rs
@@ -0,0 +1,23 @@
+#![allow(unused_variables)]
+
+type File = String;
+
+fn open(f: &mut File) -> bool {
+    true
+}
+
+fn close(f: &mut File) -> bool {
+    true
+}
+
+#[allow(dead_code)]
+fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
+    unimplemented!()
+}
+
+fn main() {
+    let mut f1 = File::from("f1.txt");
+    open(&mut f1);
+    // read(f1, vec![]);
+    close(&mut f1);
+}
