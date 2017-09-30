d39e431d3b3bc256571a2cb81332239dec85ed33
Bogdan Arabadzhi <barabadzhi@iba.by>
2017-09-30 22:52:18 +0200

    Add fileresult exercise

---
+++ fileresult/src/main.rs
@@ -0,0 +1,69 @@
+extern crate rand;
+
+use rand::Rng;
+
+fn one_in(n: u32) -> bool {
+    rand::thread_rng().gen_weighted_bool(n)
+}
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+}
+
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+        }
+    }
+
+    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+        let mut f = File::new(name);
+        f.data = data.to_owned();
+        f
+    }
+
+    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        Ok(read_length)
+    }
+}
+
+fn open(f: File) -> Result<File, String> {
+    if one_in(10_000) {
+        let err_msg = String::from("Permission denied");
+        return Err(err_msg);
+    }
+    Ok(f)
+}
+
+fn close(f: File) -> Result<File, String> {
+    if one_in(100_000) {
+        let err_msg = String::from("Interrupted by signal!");
+        return Err(err_msg);
+    }
+    Ok(f)
+}
+
+fn main() {
+    let f4_data: Vec<u8> = vec![114, 117, 115, 116, 33];
+    let mut f4 = File::new_with_data("4.txt", &f4_data);
+
+    let mut buffer: Vec<u8> = vec![];
+
+    f4 = open(f4).unwrap();
+    let f4_length = f4.read(&mut buffer).unwrap();
+    f4 = close(f4).unwrap();
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f4);
+    println!("{} is {} bytes long", &f4.name, f4_length);
+    println!("{}", text);
+}
