3ea01aee1c337e631625b556a53a45fcd10b3cf9
Bogdan Arabadzhi <barabadzhi@iba.by>
2017-09-30 22:18:20 +0200

    Improve ergonomics of defining-files-neatly exercise

--- defining-files-neatly/src/main.rs
+++ defining-files-neatly/src/main.rs
@@ -13,14 +13,43 @@ impl File {
             data: Vec::new(),
         }
     }
+
+    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+        let mut f = File::new(name);
+        f.data = data.clone();
+        f
+    }
+
+    fn read(self: &File, save_to: &mut Vec<u8>) -> usize {
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        read_length
+    }
+}
+
+fn open(f: &mut File) -> bool {
+    true
+}
+
+fn close(f: &mut File) -> bool {
+    true
 }
 
 fn main() {
-    let f3 = File::new("f3.txt");
+    let f3_data: Vec<u8> = vec![114, 117, 115, 116, 33];
+    let mut f3 = File::new_with_data("2.txt", &f3_data);
 
-    let f3_name = &f3.name;
-    let f3_length = f3.data.len();
+    let mut buffer: Vec<u8> = vec![];
+
+    open(&mut f3);
+    let f3_length = f3.read(&mut buffer);
+    close(&mut f3);
+
+    let text = String::from_utf8_lossy(&buffer);
 
     println!("{:?}", f3);
-    println!("{} is {} bytes long", f3_name, f3_length);
+    println!("{} is {} bytes long", &f3.name, f3_length);
+    println!("{}", text);
 }
