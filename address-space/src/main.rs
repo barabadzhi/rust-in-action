ab9338c33269d3ebaba61ae087bab7fb18c9284d
Bogdan Arabadzhi <bogdan.today@gmail.com>
2018-03-25 17:16:57 +0200

    Add address-space

---
+++ address-space/src/main.rs
@@ -0,0 +1,21 @@
+static GLOBAL: i32 = 1000;
+
+fn noop() -> *const i32 {
+    let noop_local = 12345;
+    &noop_local as *const i32
+}
+
+fn main() {
+    let local_str = "a";
+    let local_int = 123;
+    let boxed_str = Box::new('b');
+    let boxed_int = Box::new(789);
+    let fn_int = noop();
+
+    println!("GLOBAL:    {:p}", &GLOBAL as *const i32);
+    println!("local_str: {:p}", local_str as *const str);
+    println!("local_int: {:p}", &local_int as *const i32);
+    println!("boxed_int: {:p}", Box::into_raw(boxed_int));
+    println!("boxed_str: {:p}", Box::into_raw(boxed_str));
+    println!("fn_int:    {:p}", fn_int);
+}
